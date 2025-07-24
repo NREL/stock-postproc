6a7b097098a6b391e9765cc9b411acadca4d6863
Alex Swindler <Alex.Swindler@nrel.gov>
2025-07-24 09:23:38 -0600

    Added dynamic dashboard container

--- server/run-prefect.sh
+++ server/run-prefect.sh
@@ -3,5 +3,5 @@ docker pull minio/mc:latest
 docker pull minio/minio:RELEASE.2025-04-22T22-12-26Z
 docker pull postgres:17-alpine3.22
 docker pull prefecthq/prefect:3-latest
-docker pull python:3
-docker compose --env-file ./.env -p prefect up -d --build
+docker pull python:3.12
+CACHEBUST=$(date +%s) docker compose --env-file ./.env -p prefect up -d --build
